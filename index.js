feee3d3616b41cfa9bc7839c7b82e87089420331
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-02-17 16:45:40 -0300

    feat: settings EJS e Bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
+// Estou dizendo para o Express usar o EJS como View engine
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
+    
+    res.render("index.ejs")
 
-    res.send("Bem vindo ao meu site!");
-
+    
 });
 
 app.listen(8080, () => {
