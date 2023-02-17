36a59190d087e92f829fd0a6928f52108d9bc08f
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-02-17 17:29:08 -0300

    feat: add card to form

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get("/", (req, res) => {
     
 });
 
+app.get("/perguntar", (req, res) => {
+
+    res.render("perguntar");
+
+})
+
 app.listen(8080, () => {
     console.log("App rodando!");
 });
