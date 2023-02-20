12bcac3e936373d8e557c60cec19f3e953115659
Guilherme Augusto da Costa <gc.augusto@outlook.com>
2023-02-20 15:42:07 -0300

    feat: sending data to back-end

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas
 app.get("/", (req, res) => {
     
     res.render("index.ejs")
@@ -17,6 +22,15 @@ app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 
 })
+
+app.post("/salvarpergunta", (req, res) => {
+
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+
+    res.send("Formulario recebido! Titulo: " + titulo + " Descrição: " + descricao);
+
+});
 
 app.listen(8080, () => {
     console.log("App rodando!");
